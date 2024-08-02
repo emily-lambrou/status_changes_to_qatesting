51f987baade78d55476312beb2504ed659548083
emily-lambrou <emily-l93@hotmail.com>
2024-08-02 12:21:14 +0300

    Create graphql.py

---
+++ src/graphql.py
@@ -0,0 +1,203 @@
+from pprint import pprint
+
+import requests
+import config
+
+
+def get_repo_issues(owner, repository, status_field_name, after=None, issues=None):
+    query = """
+    query GetRepoIssues($owner: String!, $repo: String!, $status: String!, $after: String) {
+          repository(owner: $owner, name: $repo) {
+            issues(first: 100, after: $after) {
+              nodes {
+                id
+                title
+                number
+                url
+                assignees(first:100) {
+                  nodes {
+                    name
+                    email
+                    login
+                  }
+                }
+                projectItems(first: 10) {
+                  nodes {
+                    project {
+                      number
+                      title
+                    }
+                    fieldValueByName(name: $status) {
+                    // ProjectV2ItemFieldSingleSelectValue represents a single-select dropdown option that will hold the status values such as "QA Testing"
+                      ... on ProjectV2ItemFieldSingleSelectValue {
+                        id
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
+                hasPreviousPage
+              }
+              totalCount
+            }
+          }
+        }
+    """
+
+    variables = {
+        'owner': owner,
+        'repo': repository,
+        'status': status_field_name,
+        'after': after
+    }
+
+    response = requests.post(
+        config.api_endpoint,
+        json={"query": query, "variables": variables},
+        headers={"Authorization": f"Bearer {config.gh_token}"}
+    )
+
+    if response.json().get('errors'):
+        print(response.json().get('errors'))
+
+    pageinfo = response.json().get('data').get('repository').get('issues').get('pageInfo')
+    if issues is None:
+        issues = []
+    issues = issues + response.json().get('data').get('repository').get('issues').get('nodes')
+    if pageinfo.get('hasNextPage'):
+        return get_repo_issues(
+            owner=owner,
+            repository=repository,
+            after=pageinfo.get('endCursor'),
+            issues=issues,
+            status_field_name=status_field_name
+        )
+
+    return issues
+
+
+def get_project_issues(owner, owner_type, project_number, status_field_name, filters=None, after=None, issues=None, previous_statuses=None):
+    query = f"""
+    query GetProjectIssues($owner: String!, $projectNumber: Int!, $status: String!, $after: String)  {{
+          {owner_type}(login: $owner) {{
+            projectV2(number: $projectNumber) {{
+              id
+              title
+              number
+              items(first: 100,after: $after) {{
+                nodes {{
+                  id
+                  fieldValueByName(name: $status) {{
+                    ... on  ProjectV2ItemFieldSingleSelectValue {{
+                      id
+                      name
+                    }}
+                  }}
+                  content {{
+                    ... on Issue {{
+                      id
+                      title
+                      number
+                      state
+                      url
+                      assignees(first:20) {{
+                        nodes {{
+                          name
+                          email
+                          login
+                        }}
+                      }}
+                    }}
+                  }}
+                }}
+                pageInfo {{
+                endCursor
+                hasNextPage
+                hasPreviousPage
+              }}
+              totalCount
+              }}
+            }}
+          }}
+        }}
+    """
+
+    variables = {
+        'owner': owner,
+        'projectNumber': project_number,
+        'status': status_field_name,
+        'after': after
+    }
+
+    response = requests.post(
+        config.api_endpoint,
+        json={"query": query, "variables": variables},
+        headers={"Authorization": f"Bearer {config.gh_token}"}
+    )
+
+    if response.json().get('errors'):
+        print(response.json().get('errors'))
+
+    pageinfo = response.json().get('data').get(owner_type).get('projectV2').get('items').get('pageInfo')
+    if issues is None:
+        issues = []
+
+    nodes = response.json().get('data').get(owner_type).get('projectV2').get('items').get('nodes')
+
+    if filters and previous_statuses:
+        filtered_issues = []
+        for node in nodes:
+            issue_id = node['content']['id']
+            current_status = node.get('fieldValueByName', {}).get('name')
+
+            # if previous
+            
+            # Check if the status matches "QA Testing"
+            field_value = node.get('fieldValueByName', {})
+            if field_Value.get('name') == 'QA Testing':
+                filtered_issues.append(node)
+              
+        nodes = filtered_issues
+
+    issues = issues + nodes
+
+    if pageinfo.get('hasNextPage'):
+        return get_project_issues(
+            owner=owner,
+            owner_type=owner_type,
+            project_number=project_number,
+            after=pageinfo.get('endCursor'),
+            filters=filters,
+            issues=issues,
+            status_field_name=status_field_name
+        )
+
+    return issues
+
+
+def add_issue_comment(issueId, comment):
+    mutation = """
+    mutation AddIssueComment($issueId: ID!, $comment: String!) {
+        addComment(input: {subjectId: $issueId, body: $comment}) {
+            clientMutationId
+        }
+    }
+    """
+
+    variables = {
+        'issueId': issueId,
+        'comment': comment
+    }
+    response = requests.post(
+        config.api_endpoint,
+        json={"query": mutation, "variables": variables},
+        headers={"Authorization": f"Bearer {config.gh_token}"}
+    )
+    if response.json().get('errors'):
+        print(response.json().get('errors'))
+
+    return response.json().get('data')
